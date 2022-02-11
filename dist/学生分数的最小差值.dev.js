04a5749575df5e55c1ca1a75b537379062dac79c
郝荣政 <haorongzheng.hrz@bytedance.com>
2022-02-11 10:09:30 +0800

    feat:  学生分数的最小差值

---
+++ dist/学生分数的最小差值.dev.js
@@ -0,0 +1,60 @@
+"use strict";
+
+/*
+ * @Author: haorongzheng
+ * @Date: 2022-02-11 10:08:22
+ * @LastEditTime: 2022-02-11 10:08:23
+ * @LastEditors: haorongzheng
+ * @Description: 
+ * @FilePath: /leetcode/学生分数的最小差值.js
+ * 保佑代码永无bug
+ */
+
+/**
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/minimum-difference-between-highest-and-lowest-of-k-scores
+ * 
+ * 给你一个 下标从 0 开始 的整数数组 nums ，其中 nums[i] 表示第 i 名学生的分数。另给你一个整数 k 。
+ * 从数组中选出任意 k 名学生的分数，使这 k 个分数间 最高分 和 最低分 的 差值 达到 最小化 。
+ * 返回可能的 最小差值 。
+
+    示例 1：
+
+    输入：nums = [90], k = 1
+    输出：0
+    解释：选出 1 名学生的分数，仅有 1 种方法：
+    - [90] 最高分和最低分之间的差值是 90 - 90 = 0
+    可能的最小差值是 0
+    示例 2：
+
+    输入：nums = [9,4,1,7], k = 2
+    输出：2
+    解释：选出 2 名学生的分数，有 6 种方法：
+    - [9,4,1,7] 最高分和最低分之间的差值是 9 - 4 = 5
+    - [9,4,1,7] 最高分和最低分之间的差值是 9 - 1 = 8
+    - [9,4,1,7] 最高分和最低分之间的差值是 9 - 7 = 2
+    - [9,4,1,7] 最高分和最低分之间的差值是 4 - 1 = 3
+    - [9,4,1,7] 最高分和最低分之间的差值是 7 - 4 = 3
+    - [9,4,1,7] 最高分和最低分之间的差值是 7 - 1 = 6
+    可能的最小差值是 2
+     
+
+    提示：
+
+    1 <= k <= nums.length <= 1000
+    0 <= nums[i] <= 105
+
+ */
+var minimumDifference = function minimumDifference(nums, k) {
+  var n = nums.length;
+  nums.sort(function (a, b) {
+    return a - b;
+  });
+  var ans = Number.MAX_SAFE_INTEGER;
+
+  for (var i = 0; i < n - k + 1; i++) {
+    ans = Math.min(ans, nums[i + k - 1] - nums[i]);
+  }
+
+  return ans;
+};
